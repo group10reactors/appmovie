eed8b35756d06808597cc6a53eb91e90776204c8
ShamsiyyaAlakbarova <93210034+ShamsiyyaAlakbarova@users.noreply.github.com>
2023-01-05 20:20:30 +0400

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,12 @@
 import './Contact.css';
 export default function Contact() {
-    return <div> <form class="contactpage" method="post" action="http://localhost:3003/messages">
-        
-    <label for="email_btn">Email: </label><input type="email" id="email_btn" name="email" />
-    <label class="subject" for="subject_btn">Subject: </label><input type="text" id="subject_btn" name="subject"/>
-    <label class="content" for="content">Content: </label><textarea name="content" rows="10" class="message_content" placeholder="Enter the message content"></textarea>
-    <input class="submitcontact" placeholder="send" type="submit"   />
+    return <div> <h1 className='message'>Send us a message!</h1><form className="contactpage" method="post" action="http://localhost:3003/messages">
+        <div className='form'>
+    <label className='mail' for="email_btn">Email: </label><input className='email' type="email" id="email_btn" name="email" />
+    <label className="subject" for="subject_btn">Subject: </label><input className='sub' type="text" id="subject_btn" name="subject"/>
+    <label className="content" for="content">Content: </label><textarea className="message_content" name="content" rows="10" placeholder="Enter the message content"></textarea>
+    <input className='subm' placeholder="send" type="submit"   />
+    </div>
 </form>
 </div>;
   }
-  
