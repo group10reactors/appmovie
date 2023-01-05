1d601e482d5ab71c31daecf89c0fb12a8d7c7abc
ShamsiyyaAlakbarova <93210034+ShamsiyyaAlakbarova@users.noreply.github.com>
2023-01-05 20:11:16 +0400

    Update PageNotFound.jsx

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,6 +1,6 @@
 import "./PageNotFound.css";
 export default function PageNotFound() {
     return <div>
-        <img className="center" src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7888.jpg?w=2000"></img>
+        <img className="center" src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7888.jpg?w=2000" alt="error404"></img>
     </div>;
   }
