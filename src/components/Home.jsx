c85e8ef9fe46eaa00f1c08258248fef4d7c011c3
ShamsiyyaAlakbarova <93210034+ShamsiyyaAlakbarova@users.noreply.github.com>
2023-01-05 12:03:08 +0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './Home.css'
+import './Home.css';
 
 export default function Home() {
-    return <div><br></br><h1>Group 10 info</h1><br></br>
+    return <div><br /><h1>Group 10 info</h1><br />
     <table>
   <tr>
     <th>Team member</th>
@@ -12,28 +12,29 @@ export default function Home() {
   <tr>
     <td>Shamsiyya Alakbarova</td>
     <td>IT</td>
-    <td><a href="">Personal Card</a>, <a href="">Weather App</a>, <a href ="">Movie Site</a></td>
+    <td><a href="https://www.youtube.com/">Personal Card</a>, <a href="https://www.youtube.com/">Weather App</a>, <a href ="https://www.youtube.com/">Movie Site</a></td>
   </tr>
   <tr>
     <td>Tural Alizada</td>
     <td>IT</td>
-    <td><a href="">Personal Card</a>, <a href="">Weather App</a>, <a href ="">Movie Site</a></td>
+    <td><a href="https://www.youtube.com/">Personal Card</a>, <a href="https://www.youtube.com/">Weather App</a>, <a href ="https://www.youtube.com/">Movie Site</a></td>
   </tr>
   <tr>
     <td>Azer Shukurlu</td>
     <td>IT</td>
-    <td><a href="">Personal Card</a>, <a href="">Weather App</a>, <a href ="">Movie Site</a></td>
+    <td><a href="https://www.youtube.com/">Personal Card</a>, <a href="https://www.youtube.com/">Weather App</a>, <a href ="https://www.youtube.com/">Movie Site</a></td>
   </tr>
-</table><br></br>
+</table><br />
 <hr></hr>
-<p>Welcome to the movies ranking site!<br></br>In this site you will be able to see information about movies, such as runtime, genre, title, and etc. In this project we used React, HTML, CSS, JS, and JSX.<br></br>
+<p>Welcome to the movies ranking site!<br />In this site you will be able to see information about movies, such as runtime, genre, title, and etc. In this project we used React, HTML, CSS, JS, and JSX.<br />
 You can see our group info table above. Before this project we had two projects. First was related to HTML and CSS. Second was mainly about Java Script. If you want to see how our team members wrote the codes 
-for these projects, you can simply click on them, and you will be redirected to the GitHub page of the team member.<br></br><br></br>If you would like to see codes of this project, click on GitHub button below. If you would like to watch video
-about this project, click on Youtube button below.<br></br>Our team hopes that you will like it! Enjoy!<br></br><br></br>
-<form action="">
+for these projects, you can simply click on them, and you will be redirected to the GitHub page of the team member.<br /><br />If you would like to see codes of this project, click on GitHub button below. If you would like to watch video
+about this project, click on Youtube button below.<br />Our team hopes that you will like it! Enjoy!<br /><br />
+<form action="https://www.youtube.com/">
     <input type="submit" value="GitHub"/>
-</form><br></br><form action="">
+</form><br /><form action="https://www.youtube.com/">
     <input type="submit" value="Youtube" />
 </form></p>
     </div>;
   }
+  
