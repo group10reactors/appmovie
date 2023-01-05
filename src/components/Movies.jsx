1946a608ebbfd8088184b71749e127be94a33e44
ShamsiyyaAlakbarova <93210034+ShamsiyyaAlakbarova@users.noreply.github.com>
2023-01-05 12:01:26 +0400

    Update Movies.jsx

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,5 +1,29 @@
+import React, { useEffect, useState } from 'react';
 export default function Movies() {
-    return <div>Movies page</div>;
-  }
+    const [Movies, fetchMovies] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:3100/movies')
+        .then((res) => res.json())
+        .then((res) => {
+            fetchMovies(res)
+            console.log(res);
+        })
+    }, []);
+    return (
+        <>
+        {Movies.map((item) => ( 
+        <div className='movies'>
+                <img src={item.Poster_Link}></img>
+                <h2>{item.Series_Title}</h2>
+                <h2>{item.IMDB_Rating}</h2>
+                <h2>{item.Overview}</h2>
+                <h2>{item.Genre}</h2>
+                <h2>{item.Runtime}</h2>
+            </div>
+        ))}
+        </>
+    );
+}
+
 
   
