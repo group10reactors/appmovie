3553c5a09042b779e09ba58c72ac9071db27d99c
alizadeht <78111301+alizadeht@users.noreply.github.com>
2023-01-05 12:47:04 +0400

    Upload Contact.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import './Contact.css';
+export default function Contact() {
+    return <div> <form class="contactpage" method="post" action="http://localhost:3003/messages">
+        
+    <label for="email_btn">Email: </label><input type="email" id="email_btn" name="email" />
+    <label class="subject" for="subject_btn">Subject: </label><input type="text" id="subject_btn" name="subject"/>
+    <label class="content" for="content">Content: </label><textarea name="content" rows="10" class="message_content" placeholder="Enter the message content"></textarea>
+    <input placeholder="send" type="submit"   />
+</form>
+</div>;
+  }
+  
