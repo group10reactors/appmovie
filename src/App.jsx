2a42b3fd087afcf1815cd14c8cd2ce1f37f4ac79
ShamsiyyaAlakbarova <93210034+ShamsiyyaAlakbarova@users.noreply.github.com>
2023-01-06 12:19:14 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ export default function App() {
         <Routes>
             <Route path="/" element=<Home /> />
             <Route path="/movies" element=<Movies /> />
+            <Route path="/movies/:moviesId" element=<MovieInfo /> />
             <Route path="/contact" element=<Contact /> />
             <Route path="*" element=<PageNotFound /> />
         </Routes>
