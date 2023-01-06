ac4b5c837908bf3c9e0d7a9cfea8e0a6a12d37a3
ShamsiyyaAlakbarova <93210034+ShamsiyyaAlakbarova@users.noreply.github.com>
2023-01-06 12:30:05 +0400

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ export default function Contact() {
         <div className='form'>
     <label className='mail' for="email_btn">Email: </label><input className='email' type="email" id="email_btn" name="email" />
     <label className="subject" for="subject_btn">Subject: </label><input className='sub' type="text" id="subject_btn" name="subject"/>
-    <label className="content" for="content">Content: </label><textarea className="message_content" name="content" rows="10" placeholder="Enter the message content"></textarea>
+    <label className="content" for="content">Content: </label><br /><textarea className="message_content" name="content" rows="10" placeholder="Enter the message content"></textarea>
     <input className='subm' placeholder="send" type="submit"   />
     </div>
 </form>
