d68d3248f79a628d6ccd62e7538a3cad3a6de3bd
ShamsiyyaAlakbarova <93210034+ShamsiyyaAlakbarova@users.noreply.github.com>
2023-01-06 16:49:53 +0400

    Update MovieInfo.jsx

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -24,24 +24,24 @@ export default function MoviesItem() {
                 <div className="containerinfo">
                     <img src={movies.Poster_Link} alt="movies_img" />
                     <h4 className="title">{movies.Series_Title}</h4>
-                    <p className="overview">{movies.Overview}</p>
-                    <h6 className="genre">Genre: {movies.Genre}</h6>
-                    <h6 className="runtime">&#128336;&nbsp;{movies.Runtime}</h6>
-                    <h6 className="year">Released Year: {movies.Released_Year}</h6>
-                    <h6 className="certificate">Certificate: {movies.Certificate}</h6>
-                    <h6 className="imdb">&#11088;&nbsp;{movies.IMDB_Rating}</h6>
+                    <p className="overviewinfo">{movies.Overview}</p>
+                    <h6 className="genre">{movies.Genre}</h6>
+                    <h6 className="runtime">{movies.Runtime}</h6>
+                    <h6 className="year">{movies.Released_Year}</h6>
+                    <h6 className="certificate">{movies.Certificate}</h6>
+                    <h6 className="imdb">{movies.IMDB_Rating}</h6>
                     <h6 className="score">{movies.Meta_Score}</h6>
-                    <h6 className="director">Movie Director: {movies.Director}</h6>
-                    <h6 className="stars">Star 1: {movies.Star1}</h6>
-                    <h6 className="stars">Star 2: {movies.Star2}</h6>
-                    <h6 className="stars">Star 3: {movies.Star3}</h6>
-                    <h6 className="stars">Star 4: {movies.Star4}</h6>
-                    <h6 className="votes">Number of Votes: {movies.No_of_Votes}</h6>
-                    <h6 className="gross">Total Gross: {movies.Gross + " " + "$"}</h6>
+                    <h6 className="director">{movies.Director}</h6>
+                    <h6 className="stars">{movies.Star1}</h6>
+                    <h6 className="stars">{movies.Star2}</h6>
+                    <h6 className="stars">{movies.Star3}</h6>
+                    <h6 className="stars">{movies.Star4}</h6>
+                    <h6 className="votes">{movies.No_of_Votes}</h6>
+                    <h6 className="gross">{movies.Gross + "USD"}</h6>
                 </div>
                 <form action="http://localhost:3000/movies">
-                    <input class="back" type="submit" value="Back" />
+                    <input type="submit" value="Back" />
                 </form>
-            </>) : (<div>Movie is Loading...</div>)
+            </>) : (<div>Loading...</div>)
     );
 }   
